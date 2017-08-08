7c9de5075f2519b4c17b3c4d39cd40ad065322f3
dickteng <dick.teng@gmail.com>
2017-08-08 15:58:32 +0800

    completed

--- homework20170805/homework20170805/Controllers/HomeController.cs
+++ homework20170805/homework20170805/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using homework20170805.Models;
 
 namespace homework20170805.Controllers
 {
@@ -25,6 +26,23 @@ namespace homework20170805.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult ShowBookKeeping()
+        {
+            var data = new List<AccountViewModel>()
+            {
+                new AccountViewModel{Type=AccountType.收入,Date=new DateTime(2017,08,01),Amount=150},
+                new AccountViewModel{Type=AccountType.支出,Date=new DateTime(2017,08,01),Amount=350},
+                new AccountViewModel{Type=AccountType.收入,Date=new DateTime(2017,08,02),Amount=1200},
+                new AccountViewModel{Type=AccountType.支出,Date=new DateTime(2017,08,02),Amount=150},
+                new AccountViewModel{Type=AccountType.支出,Date=new DateTime(2017,08,03),Amount=2120},
+                new AccountViewModel{Type=AccountType.支出,Date=new DateTime(2017,08,03),Amount=450},
+                new AccountViewModel{Type=AccountType.支出,Date=new DateTime(2017,08,04),Amount=250},
+                new AccountViewModel{Type=AccountType.收入,Date=new DateTime(2017,08,04),Amount=170}
+            };
+
+            return View(data);
         }
     }
 }
---
+++ homework20170805/homework20170805/Models/AccountViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace homework20170805.Models
+{
+    public class AccountViewModel
+    {
+        [Display(Name ="類別")]
+        public AccountType Type { get; set; }
+        [DisplayFormat(DataFormatString ="{0:yyyy/MM/dd}")]
+        [Display(Name ="日期")]
+        public DateTime Date { get; set; }
+        [Display(Name ="金額")]
+        [DisplayFormat(DataFormatString ="{0:N0}")]
+        public int Amount { get; set; }
+    }
+
+    public enum AccountType
+    {
+        收入,
+        支出
+    }
+}
